eb47c3a55f573fbe985a738a11abf881cbc91a34
tejas.korat <tejas.korat@metizsoft.com>
2026-07-16 14:43:08 +0530

    mails error

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
 use Illuminate\Support\Facades\Schedule;
 
